dc3116d295cfb4cd17b2e389198749522d268c6e
Corey Jamison <cjamison@ualberta.ca>
2014-09-29 11:28:59 -0700

    added some signal handling and setjmp, longjmp to play with segv signals

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <signal.h>
+#include <setjmp.h>
 
 struct memchunk
 {
@@ -8,10 +10,35 @@ struct memchunk
 };
 
 int get_mem_layout( struct memchunk *chuck_list, int size );
+void try_to_segfault();
+
+static jmp_buf env;
+
+static void segv_handler( int signal )
+{
+	printf( "SEGV handled!\n" );
+	longjmp( env, 1 );
+}
 
 int main()
 {
 	printf( "CMPUT 379 Assignment 1\n" );
+
+	(void) signal( SIGSEGV, segv_handler );
+
+	if( setjmp( env ) != 0 )
+	{
+		printf( "We segfaulted!!\n" );
+	}
+	else
+	{
+		try_to_segfault();
+	}
 	return 0;
 }
 
+void try_to_segfault()
+{
+	char* bad_ptr = (char*) 0;
+	*bad_ptr = 'a';
+}
